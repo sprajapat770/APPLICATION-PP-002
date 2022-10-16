b813db921b6757c80b3a7079c6e0841151ed1ef5
Suraj Prajapat <suraj@magento360.com>
2022-10-16 19:44:49 +0530

    html added to views

--- src/views/index.php
+++ src/views/index.php
@@ -1,4 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+</head>
+<body>
 <form method="post" action="/upload" enctype="multipart/form-data">
     <input type="file" name="receipt"/>
     <button type="submit">Upload</button>
 </form>
+</body>
+</html>
