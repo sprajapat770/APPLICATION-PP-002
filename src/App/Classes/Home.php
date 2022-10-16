dc9568e7d9dbe287ceb57d951390964e29d20444
Suraj Prajapat <suraj@magento360.com>
2022-10-16 17:13:12 +0530

    get post methods

--- src/App/Classes/Home.php
+++ src/App/Classes/Home.php
@@ -6,6 +6,15 @@ class Home
 {
     public function index():string
     {
-        return 'Home';
+        echo '<pre>';
+        var_dump($_GET);
+        echo '</pre>';
+        echo '<pre>';
+        var_dump($_POST);
+        echo '</pre>';
+        return '<form method="post" action="/?foo=bar">
+            <label>Amount</label>
+            <input type="text" name="amount">
+            </form>';
     }
 }
